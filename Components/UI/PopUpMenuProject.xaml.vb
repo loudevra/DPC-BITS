6852cfdec2ca331801b9d7504f5d90e11e0e7c0a
Arione Dauis <dauis.arionejohn@gmail.com>
2025-03-08 12:53:36 +0800

    Fixed Temporal Placeholders

--- Components/UI/PopUpMenuProject.xaml.vb
+++ Components/UI/PopUpMenuProject.xaml.vb
@@ -5,6 +5,12 @@
         Public Sub New()
             InitializeComponent()
         End Sub
+
+        Private Sub NavigateToNewInvoice(sender As Object, e As RoutedEventArgs)
+            MessageBox.Show("Navigate to New Invoice", "Navigation", MessageBoxButton.OK, MessageBoxImage.Information)
+        End Sub
+
+
     End Class
 
 
