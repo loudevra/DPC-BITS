997fe53f0e408b091fda4ef08801d0c83f9ab4ff
Isaiah Luciano <isaiahreyesluciano@gmail.com>
2025-03-10 14:42:56 +0800

    Renamed Branch

--- Views/Stocks/PurchaseOrder/NewOrder.xaml.vb
+++ Views/Stocks/PurchaseOrder/NewOrder.xaml.vb
@@ -1,3 +1,13 @@
-﻿Public Class NewOrder
+﻿Namespace DPC.Views.Stocks.PurchaseOrder
+    Public Class NewOrder
+        Inherits Window
 
-End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+            MessageBox.Show("You Clicked Me!")
+        End Sub
+    End Class
+End Namespace
