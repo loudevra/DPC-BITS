a2336e19985e0a72238f19bbb97c32a490ea5f78
Clark Bernandino <clarkings14@gmail.com>
2025-03-11 12:02:10 +0800

    Resolved merge conflicts

---
+++ Components/Forms/AddNewProductsNextForm.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace DPC.Components.Forms
+    Partial Public Class AddNewProductsNextForm
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub AddNewProduct(sender As Object, e As RoutedEventArgs)
+
+            MessageBox.Show("Add New Product button clicked")
+        End Sub
+
+        Private Sub Cancel(sender As Object, e As RoutedEventArgs)
+
+            MessageBox.Show("Cancel button clicked")
+        End Sub
+    End Class
+End Namespace
