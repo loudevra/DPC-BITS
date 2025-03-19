d332f5c31f31bb0b092df3ef78c6cab92297f1a3
Clark Bernandino <clarkings14@gmail.com>
2025-03-19 11:59:07 +0800

    Revamped SignIn UI

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,10 +1,14 @@
 ﻿Imports System.Windows
 Imports System.Windows.Input
+Imports System.Windows.Media.Animation
+Imports System.Windows.Media.Effects
+Imports DPC.DPC.Data.Converters
 
 Namespace DPC
     Partial Public Class MainWindow
         Public Sub New()
             InitializeComponent()
+            PasswordMaskingBehavior.SetEnablePasswordMasking(txtPassword, True)
         End Sub
 
         ' Sign-In button click event
@@ -16,5 +20,24 @@ Namespace DPC
             dashboard.Show()
             Me.Close()
         End Sub
+
+        Private Sub BtnExit_Click(sender As Object, e As RoutedEventArgs)
+            Application.Current.Shutdown()
+        End Sub
+
+        ' Animate the Logo with Slow RGB Glow
+        Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+            Dim colorAnimation As New ColorAnimation With {
+                .From = Colors.Red,
+                .To = Colors.Blue,
+                .Duration = New Duration(TimeSpan.FromSeconds(4)),
+                .AutoReverse = True,
+                .RepeatBehavior = RepeatBehavior.Forever
+            }
+
+            Dim glowEffect As DropShadowEffect = CType(LogoImage.Effect, DropShadowEffect)
+            glowEffect.BeginAnimation(DropShadowEffect.ColorProperty, colorAnimation)
+        End Sub
+
     End Class
 End Namespace
---
+++ data/Converters/PasswordMaskingBehavior.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Windows.Controls
+Imports System.Windows
+Imports System.Windows.Input
+
+Namespace DPC.Data.Converters
+    Public Class PasswordMaskingBehavior
+        Public Shared Function GetEnablePasswordMasking(element As DependencyObject) As Boolean
+            Return element.GetValue(EnablePasswordMaskingProperty)
+        End Function
+
+        Public Shared Sub SetEnablePasswordMasking(element As DependencyObject, value As Boolean)
+            element.SetValue(EnablePasswordMaskingProperty, value)
+        End Sub
+
+        Public Shared ReadOnly EnablePasswordMaskingProperty As DependencyProperty =
+            DependencyProperty.RegisterAttached(
+                "EnablePasswordMasking",
+                GetType(Boolean),
+                GetType(PasswordMaskingBehavior),
+                New PropertyMetadata(False, AddressOf OnEnablePasswordMaskingChanged)
+            )
+
+        Private Shared Sub OnEnablePasswordMaskingChanged(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
+            Dim textBox As TextBox = TryCast(d, TextBox)
+
+            If textBox IsNot Nothing AndAlso CBool(e.NewValue) Then
+                AddHandler textBox.PreviewTextInput, AddressOf OnTextInput
+                DataObject.AddPastingHandler(textBox, AddressOf OnPaste)
+            Else
+                RemoveHandler textBox.PreviewTextInput, AddressOf OnTextInput
+                DataObject.RemovePastingHandler(textBox, AddressOf OnPaste)
+            End If
+        End Sub
+
+        Private Shared Sub OnTextInput(sender As Object, e As TextCompositionEventArgs)
+            Dim textBox As TextBox = CType(sender, TextBox)
+            textBox.Text += e.Text
+            textBox.CaretIndex = textBox.Text.Length
+            e.Handled = True
+        End Sub
+
+        Private Shared Sub OnPaste(sender As Object, e As DataObjectPastingEventArgs)
+            e.CancelCommand()
+        End Sub
+    End Class
+End Namespace
