37c14c004e709caaa72cfb7f41122b31409c4bb3
Clark Bernandino <clarkings14@gmail.com>
2025-03-20 17:35:24 +0800

    Fixed UI in ManageProducts.xaml Connect to database FetchData of Database

---
+++ data/models/ManageProducts.vb
@@ -0,0 +1,9 @@
+﻿Namespace DPC.Data.Model
+    Public Class ManageProducts
+        Public Property ID As Integer
+        Public Property Name As String
+        ' Public Property Description As String
+
+    End Class
+End Namespace
+
