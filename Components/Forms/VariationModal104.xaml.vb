a92ded393985d17012d576a69bf7b36575bd5757
Clark Bernandino <clarkings14@gmail.com>
2025-03-21 11:17:59 +0800

    Created forms for VariationModal102 VariationModal104

---
+++ Components/Forms/VariationModal104.xaml.vb
@@ -0,0 +1,18 @@
+﻿Namespace DPC.Components.Forms
+    Public Class VariationModal104
+        Public Event CloseRequested As EventHandler
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub ClosePopup(sender As Object, e As RoutedEventArgs)
+            RaiseEvent CloseRequested(Me, EventArgs.Empty)
+        End Sub
+
+        Private Sub AddSupplier(sender As Object, e As RoutedEventArgs)
+            MessageBox.Show("Supplier Added Successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information)
+            RaiseEvent CloseRequested(Me, EventArgs.Empty)
+        End Sub
+    End Class
+End Namespace
