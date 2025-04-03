ac4c9f170e12888fa2babcaf886ea08db25852a3
Arione Dauis <dauis.arionejohn@gmail.com>
2025-04-03 16:58:10 +0800

    Revert Warehouse

--- data/Controllers/WarehouseController.vb
+++ data/Controllers/WarehouseController.vb
@@ -1,122 +1,78 @@
-﻿Imports System.Data
-Imports System.IO
-Imports MySql.Data.MySqlClient
-Imports DPC.DPC.Data.Models
+﻿Imports MySql.Data.MySqlClient
 Imports System.Collections.ObjectModel
-Imports System.Convert
+Imports DPC.DPC.Data.Model
 
-Namespace DPC.Controllers
-    Public Class DocumentController
+Namespace DPC.Data.Controllers
+    Public Class WarehouseController
+        Public Shared Reload As Boolean
 
-        ' Get all documents for a specific employee
-        Public Function GetDocumentsByEmployeeID(employeeID As Integer) As ObservableCollection(Of Document)
-            Dim documents As New ObservableCollection(Of Document)()
-
-            Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
-                conn.Open()
-
-                Dim query As String = "SELECT * FROM Documents WHERE EmployeeID = @EmployeeID ORDER BY UploadDate DESC"
-                Using command As New MySqlCommand(query, conn)
-                    command.Parameters.AddWithValue("@EmployeeID", employeeID)
-
-                    Using reader As MySqlDataReader = command.ExecuteReader()
-                        While reader.Read()
-                            Dim document As New Document() With {
-                                .DocumentID = Convert.ToInt32(reader("DocumentID")),
-                                .EmployeeID = Convert.ToInt32(reader("EmployeeID")),
-                                .Title = reader("Title").ToString(),
-                                .FileName = reader("FileName").ToString(),
-                                .FileType = reader("FileType").ToString(),
-                                .FileSize = Convert.ToInt64(reader("FileSize")),
-                                .UploadDate = Convert.ToDateTime(reader("UploadDate"))
-                            }
-                            documents.Add(document)
-                        End While
+        ' Function to Fetch Business Locations from the Database
+        Public Shared Function GetBusinessLocations() As ObservableCollection(Of KeyValuePair(Of Integer, String))
+            Dim locations As New ObservableCollection(Of KeyValuePair(Of Integer, String))()
+            Try
+                ' Use connection from SplashScreen
+                Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
+                    conn.Open()
+                    Dim query As String = "SELECT locationID, locationName FROM businesslocation"
+                    Using cmd As New MySqlCommand(query, conn)
+                        Using reader As MySqlDataReader = cmd.ExecuteReader()
+                            While reader.Read()
+                                locations.Add(New KeyValuePair(Of Integer, String)(reader.GetInt32(0), reader.GetString(1)))
+                            End While
+                        End Using
                     End Using
-                End Using
-            End Using
-
-            Return documents
+                End Using ' ✅ Connection automatically returned to the pool
+            Catch ex As Exception
+                MessageBox.Show($"Error fetching business locations: {ex.Message}", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error)
+            End Try
+            Return locations
         End Function
 
-        ' Get document by ID (including file content)
-        Public Function GetDocumentByID(documentID As Integer, employeeID As Integer) As Document
-            Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
-                conn.Open()
+        ' Function to Add a Warehouse to the Database
+        Public Shared Function AddWarehouse(name As String, description As String, businessLocationID As Integer) As Boolean
+            Try
+                ' Use connection from SplashScreen
+                Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
+                    conn.Open()
+                    Dim query As String = "INSERT INTO warehouse (name, description, buisnesslocationid) VALUES (@name, @description, @businessLocationID)"
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@name", name)
+                        cmd.Parameters.AddWithValue("@description", description)
+                        cmd.Parameters.AddWithValue("@businessLocationID", If(businessLocationID = 0, DBNull.Value, businessLocationID))
+                        Dim result As Integer = cmd.ExecuteNonQuery()
+                        Return result > 0
 
-                Dim query As String = "SELECT * FROM Documents WHERE DocumentID = @DocumentID AND EmployeeID = @EmployeeID"
-                Using command As New MySqlCommand(query, conn)
-                    command.Parameters.AddWithValue("@DocumentID", documentID)
-                    command.Parameters.AddWithValue("@EmployeeID", employeeID)
-
-                    Using reader As MySqlDataReader = command.ExecuteReader()
-                        If reader.Read() Then
-                            Dim document As New Document() With {
-                                .DocumentID = Convert.ToInt32(reader("DocumentID")),
-                                .EmployeeID = Convert.ToInt32(reader("EmployeeID")),
-                                .Title = reader("Title").ToString(),
-                                .FileName = reader("FileName").ToString(),
-                                .FileContent = reader("FileContent").ToString(),
-                                .FileType = reader("FileType").ToString(),
-                                .FileSize = Convert.ToInt64(reader("FileSize")),
-                                .UploadDate = Convert.ToDateTime(reader("UploadDate"))
-                            }
-                            Return document
-                        End If
                     End Using
-                End Using
-            End Using
-
-            Return Nothing
+                End Using ' ✅ Connection automatically returned to the pool
+            Catch ex As Exception
+                MessageBox.Show($"Error adding warehouse: {ex.Message}", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error)
+                Return False
+            End Try
         End Function
 
-        ' Add a new document
-        Public Function AddDocument(document As Document) As Boolean
-            Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
-                conn.Open()
-
-                Dim query As String = "INSERT INTO Documents (EmployeeID, Title, FileName, FileContent, FileType, FileSize, UploadDate) " &
-                                    "VALUES (@EmployeeID, @Title, @FileName, @FileContent, @FileType, @FileSize, @UploadDate)"
-
-                Using command As New MySqlCommand(query, conn)
-                    command.Parameters.AddWithValue("@EmployeeID", document.EmployeeID)
-                    command.Parameters.AddWithValue("@Title", document.Title)
-                    command.Parameters.AddWithValue("@FileName", document.FileName)
-                    command.Parameters.AddWithValue("@FileContent", document.FileContent)
-                    command.Parameters.AddWithValue("@FileType", document.FileType)
-                    command.Parameters.AddWithValue("@FileSize", document.FileSize)
-                    command.Parameters.AddWithValue("@UploadDate", document.UploadDate)
-
-                    Return command.ExecuteNonQuery() > 0
-                End Using
-            End Using
+        ' Function to Fetch Warehouses Data
+        Public Shared Function GetWarehouses() As ObservableCollection(Of Warehouses)
+            Dim warehouseList As New ObservableCollection(Of Warehouses)()
+            Try
+                ' Use connection from SplashScreen
+                Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
+                    conn.Open()
+                    Dim query As String = "SELECT warehouseid, name, description FROM warehouse;"
+                    Using cmd As New MySqlCommand(query, conn)
+                        Using reader As MySqlDataReader = cmd.ExecuteReader()
+                            While reader.Read()
+                                warehouseList.Add(New Warehouses With {
+                                    .ID = reader.GetInt32("warehouseid"),
+                                    .Name = reader.GetString("name"),
+                                    .Description = reader.GetString("description")})
+                            End While
+                        End Using
+                    End Using
+                End Using ' ✅ Connection automatically returned to the pool
+            Catch ex As Exception
+                MessageBox.Show("Error fetching Warehouses: " & ex.Message, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error)
+            End Try
+            Return warehouseList
         End Function
-
-        ' Delete a document
-        Public Function DeleteDocument(documentID As Integer, employeeID As Integer) As Boolean
-            Using conn As MySqlConnection = SplashScreen.GetDatabaseConnection()
-                conn.Open()
-
-                Dim query As String = "DELETE FROM Documents WHERE DocumentID = @DocumentID AND EmployeeID = @EmployeeID"
-                Using command As New MySqlCommand(query, conn)
-                    command.Parameters.AddWithValue("@DocumentID", documentID)
-                    command.Parameters.AddWithValue("@EmployeeID", employeeID)
-
-                    Return command.ExecuteNonQuery() > 0
-                End Using
-            End Using
-        End Function
-
-        ' Helper method to convert file to Base64
-        Public Shared Function FileToBase64(filePath As String) As String
-            Dim bytes As Byte() = File.ReadAllBytes(filePath)
-            Return Convert.ToBase64String(bytes)
-        End Function
-
-        ' Helper method to save Base64 to file
-        Public Shared Sub Base64ToFile(base64String As String, outputPath As String)
-            Dim bytes As Byte() = Convert.FromBase64String(base64String)
-            File.WriteAllBytes(outputPath, bytes)
-        End Sub
     End Class
 End Namespace
