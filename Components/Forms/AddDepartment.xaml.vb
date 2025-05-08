f4dfabbf37c992fd16d38340a4faf388c5a9b19d
Clark Bernandino <clarkings14@gmail.com>
2025-05-08 16:38:39 +0800

    Added  Department UI and Popup. Also Redirect #69

---
+++ Components/Forms/AddDepartment.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports ClosedXML.Excel
+Imports DPC.DPC.Data.Controllers
+Imports DPC.DPC.Data.Model
+Imports DPC.DPC.Data.Models
+Imports DPC.DPC.Data.Helpers
+Imports Microsoft.Win32
+Imports System.Collections.ObjectModel
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Reflection
+Imports System.Windows.Controls.Primitives
+
+Namespace DPC.Components.Forms
+    Public Class AddDepartment
+        Inherits UserControl
+
+        ' Properties for pagination
+        Private _paginationHelper As PaginationHelper
+        Private _searchFilterHelper As SearchFilterHelper
+
+        ' UI elements for direct access
+        Private popup As Popup
+        Private recentlyClosed As Boolean = False
+
+        Public Sub New()
+            InitializeComponent()
+            InitializeControls()
+        End Sub
+
+        Private Sub InitializeControls()
+
+            ' Initialize and load product categories data
+            ' LoadData()
+        End Sub
+    End Class
+End Namespace
