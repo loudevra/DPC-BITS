1d7bca285076042e025b1f37d1b3805299bb8b93
Isaiah Luciano <isaiahreyesluciano@gmail.com>
2025-05-09 11:50:46 +0800

    optimized functions for buttons

--- Views/POS/POSForm.xaml.vb
+++ Views/POS/POSForm.xaml.vb
@@ -8,6 +8,7 @@ Imports DPC.DPC.Data.Helpers
 Imports DPC.DPC.Data.Model
 Imports MaterialDesignThemes.Wpf
 Imports Microsoft.Win32
+Imports NuGet.Protocol.Plugins
 
 Namespace DPC.Views.POS
     Public Class POSForm
@@ -20,83 +21,52 @@ Namespace DPC.Views.POS
             OpenSettings(Nothing, Nothing)
         End Sub
 
-        Private Sub OpenSettings(sender As Object, e As RoutedEventArgs)
-            If StackPanelSettings.Visibility = Visibility.Visible Then
+        Private Sub OpenContent(sender As Object, e As RoutedEventArgs,
+                                OpenStackPanel As StackPanel, OpenIcon As PackIcon, OpenText As TextBlock,
+                                CloseStackPanel As StackPanel, CloseIcon As PackIcon, CloseText As TextBlock,
+                                CloseStackPanel2 As StackPanel, CloseIcon2 As PackIcon, CloseText2 As TextBlock)
+            If OpenStackPanel.Visibility = Visibility.Visible Then
+                OpenIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
+                OpenText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
 
-                SettingsIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                SettingsText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                StackPanelSettings.Visibility = Visibility.Collapsed
-                GridSettings.Visibility = Visibility.Collapsed
-                StackPanelCoupon.Visibility = Visibility.Collapsed
+                OpenStackPanel.Visibility = Visibility.Collapsed
+                CloseStackPanel.Visibility = Visibility.Collapsed
+                CloseStackPanel2.Visibility = Visibility.Collapsed
             Else
+                OpenIcon.Foreground = New BrushConverter().ConvertFromString("#555555")
+                OpenText.Foreground = New BrushConverter().ConvertFromString("#555555")
 
-                SettingsIcon.Foreground = New BrushConverter().ConvertFromString("#555555")
-                SettingsText.Foreground = New BrushConverter().ConvertFromString("#555555")
+                CloseIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
+                CloseText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
 
-                InvoiceIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                InvoiceText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
+                CloseIcon2.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
+                CloseText2.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
 
-                CouponIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                CouponText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                StackPanelSettings.Visibility = Visibility.Visible
-                GridSettings.Visibility = Visibility.Collapsed
-                StackPanelCoupon.Visibility = Visibility.Collapsed
+                OpenStackPanel.Visibility = Visibility.Visible
+                CloseStackPanel.Visibility = Visibility.Collapsed
+                CloseStackPanel2.Visibility = Visibility.Collapsed
             End If
 
         End Sub
 
+        Private Sub OpenSettings(sender As Object, e As RoutedEventArgs)
+            OpenContent(sender, e,
+                        StackPanelSettings, SettingsIcon, SettingsText,
+                        StackPanelInvoice, InvoiceIcon, InvoiceText,
+                        StackPanelCoupon, CouponIcon, CouponText)
+        End Sub
         Private Sub OpenProperties(sender As Object, e As RoutedEventArgs)
-            If GridSettings.Visibility = Visibility.Visible Then
-
-                InvoiceIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                InvoiceText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                StackPanelSettings.Visibility = Visibility.Collapsed
-                GridSettings.Visibility = Visibility.Collapsed
-                StackPanelCoupon.Visibility = Visibility.Collapsed
-            Else
-
-                SettingsIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                SettingsText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                InvoiceIcon.Foreground = New BrushConverter().ConvertFromString("#555555")
-                InvoiceText.Foreground = New BrushConverter().ConvertFromString("#555555")
-
-                CouponIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                CouponText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                StackPanelSettings.Visibility = Visibility.Collapsed
-                GridSettings.Visibility = Visibility.Visible
-                StackPanelCoupon.Visibility = Visibility.Collapsed
-            End If
+            OpenContent(sender, e,
+            StackPanelInvoice, InvoiceIcon, InvoiceText,
+            StackPanelSettings, SettingsIcon, SettingsText,
+            StackPanelCoupon, CouponIcon, CouponText)
         End Sub
 
         Private Sub OpenCoupon(sender As Object, e As RoutedEventArgs)
-            If StackPanelCoupon.Visibility = Visibility.Visible Then
-
-                CouponIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                CouponText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                StackPanelSettings.Visibility = Visibility.Collapsed
-                GridSettings.Visibility = Visibility.Collapsed
-                StackPanelCoupon.Visibility = Visibility.Collapsed
-            Else
-
-                SettingsIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                SettingsText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                InvoiceIcon.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-                InvoiceText.Foreground = New BrushConverter().ConvertFromString("#AEAEAE")
-
-                CouponIcon.Foreground = New BrushConverter().ConvertFromString("#555555")
-                CouponText.Foreground = New BrushConverter().ConvertFromString("#555555")
-
-                StackPanelSettings.Visibility = Visibility.Collapsed
-                GridSettings.Visibility = Visibility.Collapsed
-                StackPanelCoupon.Visibility = Visibility.Visible
-            End If
+            OpenContent(sender, e,
+            StackPanelCoupon, CouponIcon, CouponText,
+            StackPanelSettings, SettingsIcon, SettingsText,
+            StackPanelInvoice, InvoiceIcon, InvoiceText)
         End Sub
     End Class
 End Namespace
