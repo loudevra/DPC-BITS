35acb4ca27923ef4a00213242c4879b8db6417a0
Clark Bernandino <clarkings14@gmail.com>
2025-05-09 15:31:37 +0800

    Added Attendace Redirection and AddAttendance Popup UI And its Redirection #73

--- Views/HRM/Employees/Attendance/AttendanceEmployee.xaml.vb
+++ Views/HRM/Employees/Attendance/AttendanceEmployee.xaml.vb
@@ -1,3 +1,24 @@
-﻿Public Class AttendanceEmployee
+﻿
+Namespace DPC.Views.HRM.Employees.Attendance
+    ''' <summary>
+    ''' Interaction logic for ProductCategories.xaml
+    ''' </summary>
+    Public Class AttendanceEmployee
+        Inherits UserControl
+        ' UI elements for direct access
+        'Private popup As Popup
+        Private recentlyClosed As Boolean = False
 
-End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        Private Sub AddAttendanceControl(sender As Object, e As RoutedEventArgs)
+            ' Create an instance of the AddCategory form
+            Dim addAttendanceControl As New DPC.Components.Forms.AddAttendance()
+            ' Get the parent window to center the popup
+            Dim parentWindow = Window.GetWindow(Me)
+            ' Open the popup
+            PopupHelper.OpenPopupWithControl(sender, addAttendanceControl, "windowcenter", True, -50, 0, parentWindow)
+        End Sub
+    End Class
+End Namespace
