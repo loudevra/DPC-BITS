55cd497035cf8277ae9e9583c5d33b54685ecee0
usermay <normaypangan19@gmail.com>
2025-06-09 15:29:55 +0800

    Feature Request: CRM Client Management Interface Groups.Support UI #106

---
+++ Views/CRM/SupportTicket.xaml.vb
@@ -0,0 +1,13 @@
+﻿Namespace DPC.Views.CRM
+    Public Class SupportTicket
+        Private Sub New()
+
+            ' This call is required by the designer.
+            InitializeComponent()
+
+            ' Add any initialization after the InitializeComponent() call.
+
+        End Sub
+    End Class
+End Namespace
+
