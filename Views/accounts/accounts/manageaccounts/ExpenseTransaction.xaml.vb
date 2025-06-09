a566645bbc95f88b86137f46029f0699ffa1ee5d
usermay <normaypangan19@gmail.com>
2025-06-09 15:08:44 +0800

    Added new files

---
+++ Views/accounts/accounts/manageaccounts/ExpenseTransaction.xaml.vb
@@ -0,0 +1,11 @@
+﻿Namespace DPC.Views.Accounts.Accounts.ManageAccounts
+    Public Class ExpenseTransaction
+        Private Sub New()
+            ' This call is required by the designer.
+            InitializeComponent()
+
+            ' Add any initialization after the InitializeComponent() call.
+        End Sub
+    End Class
+
+End Namespace
