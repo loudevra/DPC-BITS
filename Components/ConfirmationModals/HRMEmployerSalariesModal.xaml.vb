40de991bffef4f9922933266b6e408bbeaa3eebb
Clark Bernandino <clarkings14@gmail.com>
2025-06-10 10:58:40 +0800

    added new 3 files

---
+++ Components/ConfirmationModals/HRMEmployerSalariesModal.xaml.vb
@@ -0,0 +1,9 @@
+﻿Namespace DPC.Components.ConfirmationModals
+    Public Class HRMEmployerSalariesModal
+        Private Sub New()
+
+            InitializeComponent()
+
+        End Sub
+    End Class
+End Namespace
