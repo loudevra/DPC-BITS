bae58a2496be60279964d557513d239790023732
Clark Bernandino <clarkings14@gmail.com>
2025-06-10 11:41:17 +0800

    Repushing committed files to dev into main-deployment

---
+++ Views/CRM/CRMClientTabs.xaml.vb
@@ -0,0 +1,10 @@
+﻿Namespace DPC.Views.CRM
+
+    Partial Public Class CRMClientTabs
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
+
---
+++ Views/CRM/CRMTabStructured.xaml.vb
@@ -0,0 +1,10 @@
+﻿Namespace DPC.Views.CRM
+
+    Partial Public Class CRMTabStructured
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
+
