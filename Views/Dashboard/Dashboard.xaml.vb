376abc66763417ec99d6a567c6ea56c3ee2652e2
usermay <normaypangan19@gmail.com>
2025-06-11 13:59:20 +0800

    Enhancement of UI T.1 #198

--- Views/Dashboard/Dashboard.xaml.vb
+++ Views/Dashboard/Dashboard.xaml.vb
@@ -1,22 +1,9 @@
-﻿
-Namespace DPC.Views.Dashboard
+﻿Namespace DPC.Views.Dashboard
     Partial Public Class Dashboard
         Inherits UserControl
 
         Public Sub New()
             InitializeComponent()
-        End Sub
-
-        Private Sub RecentInvoicesDataGrid_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
-
-        End Sub
-
-        Private Sub RecentInvoicesDataGrid_SelectionChanged_1(sender As Object, e As SelectionChangedEventArgs)
-
-        End Sub
-
-        Private Sub cmbCashflowType_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cmbCashflowType.SelectionChanged
-
         End Sub
     End Class
 End Namespace
