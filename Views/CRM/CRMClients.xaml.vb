3266a4a6f2cd40674e2ab6bfae23b38d0b08a630
usermay <normaypangan19@gmail.com>
2025-06-12 09:03:47 +0800

    Enhance UI T.1 revisions #214

--- Views/CRM/CRMClients.xaml.vb
+++ Views/CRM/CRMClients.xaml.vb
@@ -1,7 +1,13 @@
 ﻿Namespace DPC.Views.CRM
     Public Class CRMClients
         Public Sub New()
+
+            ' This call is required by the designer.
             InitializeComponent()
+
+            ' Add any initialization after the InitializeComponent() call.
+
         End Sub
     End Class
 End Namespace
+
