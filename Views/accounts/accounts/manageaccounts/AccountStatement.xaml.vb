78b323e7db728f6c78422ae664ea294d2a663ca0
Chrestine Hiangan <chrestinehiangan15@gmail.com>
2025-06-13 16:03:10 +0800

    UPDATED ACCOUNT UI

---
+++ Views/accounts/accounts/manageaccounts/AccountStatement.xaml.vb
@@ -0,0 +1,7 @@
+﻿Namespace DPC.Views.Accounts.Accounts.ManageAccounts
+    Public Class AccountStatement
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
