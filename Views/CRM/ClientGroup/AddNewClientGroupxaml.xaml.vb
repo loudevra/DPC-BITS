84729b4f70b764ca990e9fc4fdb2ba0f8e4a479b
usermay <normaypangan19@gmail.com>
2025-06-17 17:47:57 +0800

    Ploshing random modules. #318

---
+++ Views/CRM/ClientGroup/AddNewClientGroupxaml.xaml.vb
@@ -0,0 +1,13 @@
+﻿Namespace DPC.Views.CRM.ClientGroup
+    Public Class AddNewClientGroup
+        Inherits UserControl
+        Public Sub New()
+
+            ' This call is required by the designer.
+            InitializeComponent()
+
+            ' Add any initialization after the InitializeComponent() call.
+
+        End Sub
+    End Class
+End Namespace
