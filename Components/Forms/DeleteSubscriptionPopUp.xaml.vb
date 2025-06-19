ab463ee6d9d722530108d2b136ac60b7f1f2f45c
usermay <normaypangan19@gmail.com>
2025-06-19 12:53:27 +0800

    Remove the Click="ConfirmDeletion_Click" and Click="CloseDeleteDialog" in Components.Forms Delete Confirmation. It encountered error since it doesn't have any function for backend.

--- Components/Forms/DeleteSubscriptionPopUp.xaml.vb
+++ Components/Forms/DeleteSubscriptionPopUp.xaml.vb
@@ -8,5 +8,9 @@
             ' Add any initialization after the InitializeComponent() call.
 
         End Sub
+
+        Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        End Sub
     End Class
 End Namespace
