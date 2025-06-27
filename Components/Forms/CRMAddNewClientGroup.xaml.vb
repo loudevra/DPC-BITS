0e29abd450d8cb0b527cc58abecf1f6f6aa25216
Chrestine Hiangan <chrestinehiangan15@gmail.com>
2025-06-27 12:13:55 +0800

    Navigation of Client Group Pop Out

--- Components/Forms/CRMAddNewClientGroup.xaml.vb
+++ Components/Forms/CRMAddNewClientGroup.xaml.vb
@@ -1,9 +1,39 @@
-﻿Namespace DPC.Components.Forms
+﻿Imports System.Windows.Controls.Primitives
+Imports DPC.DPC.Data.Controllers
 
-    Partial Public Class CRMAddNewClientGroup
+Namespace DPC.Components.Forms
+
+    Public Class CRMAddNewClientGroup
+        Inherits UserControl
 
         Public Sub New()
             InitializeComponent()
+            AddHandler BtnClose.Click, AddressOf BtnClose_Click
         End Sub
+
+        ' Close Button Handler
+        Private Sub BtnClose_Click(sender As Object, e As RoutedEventArgs)
+            Dim parent = TryCast(Me.Parent, ContentControl)
+            If parent IsNot Nothing Then
+                Dim container = TryCast(parent.Parent, Panel)
+                If container IsNot Nothing Then
+                    container.Children.Remove(parent)
+                End If
+            Else
+                Dim parentPopup = TryCast(Me.Parent, Popup)
+                If parentPopup IsNot Nothing Then
+                    parentPopup.IsOpen = False
+                Else
+                    Dim parentWindow = Window.GetWindow(Me)
+                    If parentWindow IsNot Nothing Then
+                        parentWindow.Close()
+                    End If
+                End If
+            End If
+        End Sub
+
     End Class
 End Namespace
+
+
+
