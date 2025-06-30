f2d7dab6e7e167bc12772d3db93a9d366b729112
Clark Bernandino <clarkings14@gmail.com>
2025-06-30 09:09:12 +0800

    Revert "Merge pull request #362 from Dream-PC-Build-and-I-T-Solutions/feature/DatePicker_CustomerDetails_Ui_FIX"
    This reverts commit b8a678629479e17347b17da9166054394d19a84d, reversing
    changes made to 32f81d0721d8837a76416333bb82a6e10a9bade9.

--- Views/accounts/accounts/CustomerDetails/CustomerDetailsMain.xaml.vb
+++ Views/accounts/accounts/CustomerDetails/CustomerDetailsMain.xaml.vb
@@ -1,53 +1,7 @@
 ﻿Namespace DPC.Views.Accounts.Accounts
     Public Class CustomerDetailsMain
-        Inherits UserControl ' ✅ Ensure this is inheriting from UserControl (if not already in designer)
-
         Public Sub New()
             InitializeComponent()
-
-            ' ✅ Automatically load Personal Information on view open
-            customerdetailsmaincontent.Content = New CustomerDetailsPersonalInformation()
-        End Sub
-
-        ' ✅ These must have Handles if defined in XAML
-        Private Sub BtnInvoice_Click(sender As Object, e As RoutedEventArgs) Handles BtnInvoice.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsInvoices()
-        End Sub
-
-        Private Sub BtnInfo_Click(sender As Object, e As RoutedEventArgs) Handles BtnInfo.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsPersonalInformation()
-        End Sub
-
-        Private Sub BtnTransactions_Click(sender As Object, e As RoutedEventArgs) Handles BtnTransactions.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsTransaction()
-        End Sub
-
-        Private Sub BtnQuotes_Click(sender As Object, e As RoutedEventArgs) Handles BtnQuotes.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsQuotes()
-        End Sub
-
-        Private Sub BtnSubscriptions_Click(sender As Object, e As RoutedEventArgs) Handles BtnSubscriptions.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsSubscriptions()
-        End Sub
-
-        Private Sub BtnProjects_Click(sender As Object, e As RoutedEventArgs) Handles BtnProjects.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsProjects()
-        End Sub
-
-        Private Sub BtnNotes_Click(sender As Object, e As RoutedEventArgs) Handles BtnNotes.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsAccountStatement()
-        End Sub
-
-        Private Sub BtnDocuments_Click(sender As Object, e As RoutedEventArgs) Handles BtnDocuments.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsAccountStatement()
-        End Sub
-
-        Private Sub BtnAccountStatement_Click(sender As Object, e As RoutedEventArgs) Handles BtnAccountStatement.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsAccountStatement()
-        End Sub
-
-        Private Sub DetailsWallet_Click(sender As Object, e As RoutedEventArgs) Handles CustomerDetailsWallet.Click
-            customerdetailsmaincontent.Content = New CustomerDetailsWallet()
         End Sub
     End Class
 End Namespace
--- Views/accounts/accounts/CustomerDetails/CustomersDetailsCards1/CustomerDetailsAccountStatement.xaml.vb
+++ Views/accounts/accounts/CustomerDetails/CustomersDetailsCards1/CustomerDetailsAccountStatement.xaml.vb
@@ -1,65 +1,8 @@
-﻿Imports DPC.DPC.Data.Controllers
-Imports System.Windows.Controls.Primitives
-Imports System.Windows.Data
-
-Namespace DPC.Views.Accounts.Accounts.CustomerDetails.CustomerDetailsCards1
+﻿Namespace DPC.Views.Accounts.Accounts.CustomerDetails.CustomerDetailsCards1
 
     Public Class CustomerDetailsAccountStatement
-        Inherits UserControl
-
-        Private fromDateViewModel As New CalendarController.SingleCalendar()
-        Private toDateViewModel As New CalendarController.SingleCalendar()
-
         Public Sub New()
             InitializeComponent()
-            SetupControllerReferences()
         End Sub
-
-        ' Setup DataContext bindings between pickers and buttons with the ViewModels
-        Private Sub SetupControllerReferences()
-            fromDateViewModel.SelectedDate = Nothing
-            toDateViewModel.SelectedDate = Nothing
-
-            FromDatePicker.DataContext = fromDateViewModel
-            FromDateButton.DataContext = fromDateViewModel
-
-            ToDatePicker.DataContext = toDateViewModel
-            ToDateButton.DataContext = toDateViewModel
-        End Sub
-
-        ' Open From Date calendar dropdown on button click
-        Private Sub FromDateButton_Click(sender As Object, e As RoutedEventArgs)
-            FromDatePicker.IsDropDownOpen = True
-        End Sub
-
-        ' Open To Date calendar dropdown on button click
-        Private Sub ToDateButton_Click(sender As Object, e As RoutedEventArgs)
-            ToDatePicker.IsDropDownOpen = True
-        End Sub
-
-        ' Update FromDateViewModel.SelectedDate on DatePicker selection change
-        Private Sub FromDatePicker_SelectedDateChanged(sender As Object, e As SelectionChangedEventArgs) Handles FromDatePicker.SelectedDateChanged
-            Dim datePicker As DatePicker = TryCast(sender, DatePicker)
-            If datePicker IsNot Nothing AndAlso datePicker.DataContext IsNot Nothing Then
-                Dim vm As CalendarController.SingleCalendar = TryCast(datePicker.DataContext, CalendarController.SingleCalendar)
-                If vm IsNot Nothing Then
-                    vm.SelectedDate = datePicker.SelectedDate
-                    BindingOperations.GetBindingExpression(FromDateButton, Button.DataContextProperty)?.UpdateTarget()
-                End If
-            End If
-        End Sub
-
-        ' Update ToDateViewModel.SelectedDate on DatePicker selection change
-        Private Sub ToDatePicker_SelectedDateChanged(sender As Object, e As SelectionChangedEventArgs) Handles ToDatePicker.SelectedDateChanged
-            Dim datePicker As DatePicker = TryCast(sender, DatePicker)
-            If datePicker IsNot Nothing AndAlso datePicker.DataContext IsNot Nothing Then
-                Dim vm As CalendarController.SingleCalendar = TryCast(datePicker.DataContext, CalendarController.SingleCalendar)
-                If vm IsNot Nothing Then
-                    vm.SelectedDate = datePicker.SelectedDate
-                    BindingOperations.GetBindingExpression(ToDateButton, Button.DataContextProperty)?.UpdateTarget()
-                End If
-            End If
-        End Sub
-
     End Class
 End Namespace
