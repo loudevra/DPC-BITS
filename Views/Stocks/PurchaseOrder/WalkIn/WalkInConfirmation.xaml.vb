bcde8baede9b98501a977b75f23423e7c496675d
bsaintjoseph <sanjose.bradley06.18@gmail.com>
2025-06-30 15:02:48 +0800

    Created UI for WalkIn Clients

---
+++ Views/Stocks/PurchaseOrder/WalkIn/WalkInConfirmation.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows
+
+
+Namespace DPC.Views.Stocks.PurchaseOrder.WalkIn
+    Partial Public Class WalkInConfirmation
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
