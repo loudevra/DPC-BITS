98354f1bb71708b89f0488f4f0f0fe82f0f45291
usermay <normaypangan19@gmail.com>
2025-07-04 15:25:53 +0800

    fixed date picker working

--- Views/Sales/POSSales/ManagePOSInvoices.xaml.vb
+++ Views/Sales/POSSales/ManagePOSInvoices.xaml.vb
@@ -1,8 +1,17 @@
-﻿Namespace DPC.Views.Sales.POSSales
+﻿Imports DPC.DPC.Data.Controllers
+
+Namespace DPC.Views.Sales.POSSales
     Public Class ManagePOSInvoices
+        Public Property StartDate As New CalendarController.SingleCalendar()
         Public Sub New()
+            StartDate.SelectedDate = Date.Today
+
+            DataContext = Me
             ' This call is required by the designer.
             InitializeComponent()
+        End Sub
+        Private Sub StartDate_Click(sender As Object, e As RoutedEventArgs)
+            StartDatePicker.IsDropDownOpen = True
         End Sub
     End Class
 End Namespace
