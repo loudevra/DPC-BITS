993b760e81425602828f2275033995af2db3a252
CarloVertulfo18 <cvertulfo04@gmail.com>
2025-11-11 17:05:08 +0800

    Quote Pagination

---
+++ Views/Sales/Quotes/ManageFiles.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.ComponentModel
+Imports System.Windows.Controls
+Imports System.Windows.Controls.Primitives
+Imports MongoDB.Driver
+Imports MongoDB.Bson
+Imports DPC.DPC.Data.Helpers
+Imports DPC.DPC.Data.Model
+
+
+Public Class ManageFiles
+        
+    End Class
