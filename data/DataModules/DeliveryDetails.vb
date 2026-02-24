1a8f8da6d70813c4a2f99b76638f20134e4bc74b
Andrews Leeo Matienzo <matienzoandrews@gmail.com>
2026-02-24 09:43:55 +0800

    clearing function for delivery details was added

--- data/DataModules/DeliveryDetails.vb
+++ data/DataModules/DeliveryDetails.vb
@@ -16,4 +16,27 @@
 
     ' Product Data (Cached from the Billing Table)
     Public Property DRDeliveryItems As List(Of Dictionary(Of String, String))
+
+    Public Sub ClearDeliveryDetails()
+        ' Reset Strings to Empty
+        DRNumber = ""
+        DRReferenceInvoice = ""
+        DRDate = ""
+        DRClientDetails = ""
+        DRClientName = ""
+        DRDeliveryNotes = ""
+        DRApprovedBy = ""
+        DRPaymentTerm = ""
+
+        ' Reset Variants/Objects
+        DRShippingMethod = Nothing
+
+        ' Re-initialize the List to wipe any cached product data
+        If DRDeliveryItems IsNot Nothing Then
+            DRDeliveryItems.Clear()
+        Else
+            DRDeliveryItems = New List(Of Dictionary(Of String, String))()
+        End If
+    End Sub
+
 End Module
