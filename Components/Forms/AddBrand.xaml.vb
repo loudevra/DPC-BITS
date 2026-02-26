ab98786c47565887d324bf71a807ad1f9e34bce5
celine-gonzales <gonzalesceline14@gmail.com>
2026-02-26 12:19:29 +0800

    add brand changes

--- Components/Forms/AddBrand.xaml.vb
+++ Components/Forms/AddBrand.xaml.vb
@@ -6,26 +6,26 @@ Namespace DPC.Components.Forms
     Public Class AddBrand
         Public Event BrandAdded()
 
+        Private ProductController As New ProductController()
+
         Public Sub New()
             InitializeComponent()
+            ProductController.GetProductCategory(CmbCategory)
         End Sub
 
         Private Sub BtnAddBrand(sender As Object, e As RoutedEventArgs)
             BrandController.InsertBrand(TxtBrand.Text)
-            RaiseEvent BrandAdded() ' Notify that a brand was added
+            RaiseEvent BrandAdded()
         End Sub
 
         Private Sub TxtBrand_TextChanged(sender As Object, e As TextChangedEventArgs)
             Dim tb = TryCast(sender, TextBox)
             If tb Is Nothing Then Return
-
             Dim original = tb.Text
             Dim upper = original.ToUpperInvariant()
             If original = upper Then Return
-
             Dim selStart = tb.SelectionStart
             Dim selLength = tb.SelectionLength
-
             RemoveHandler tb.TextChanged, AddressOf TxtBrand_TextChanged
             tb.Text = upper
             tb.SelectionStart = Math.Min(selStart, tb.Text.Length)
