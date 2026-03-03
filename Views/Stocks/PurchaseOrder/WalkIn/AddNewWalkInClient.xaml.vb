60e9e784709189a3ce826d3473158460ef6c6140
Andrews Leeo Matienzo <matienzoandrews@gmail.com>
2026-03-03 10:02:07 +0800

    Add new client fixed

--- Views/Stocks/PurchaseOrder/WalkIn/AddNewWalkInClient.xaml.vb
+++ Views/Stocks/PurchaseOrder/WalkIn/AddNewWalkInClient.xaml.vb
@@ -1,9 +1,56 @@
-﻿Namespace DPC.Views.Stocks.PurchaseOrder.WalkIn
+﻿Imports DPC.DPC.Data.Controllers
+Imports DPC.DPC.Data.Helpers.ViewLoader
+Imports DPC.DPC.Data.Models
+
+Namespace DPC.Views.Stocks.PurchaseOrder.WalkIn
     Partial Public Class AddNewWalkInClient
         Inherits UserControl
 
         Public Sub New()
             InitializeComponent()
+        End Sub
+
+        Private Sub AddClient(sender As Object, e As RoutedEventArgs)
+            ' Check required fields
+            If String.IsNullOrEmpty(txtClientName.Text) OrElse
+               String.IsNullOrEmpty(txtPhoneNumber.Text) OrElse
+               String.IsNullOrEmpty(txtClientEmail.Text) Then
+                MessageBox.Show("Please fill in all required fields.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning)
+                Exit Sub
+            End If
+
+            Dim completeAddress = $"{txtAddress.Text}, {txtCity.Text}, {txtRegion.Text}, {txtCountry.Text}, {txtPostalCode.Text}"
+            ' Create the Client object (Make sure to use commas correctly!)
+            Dim client As New Client With {
+                .ClientGroupID = 2,
+                .Name = txtClientName.Text,
+                .Phone = txtPhoneNumber.Text,
+                .Email = txtClientEmail.Text,
+                .BillingAddress = completeAddress,
+                .ShippingAddress = completeAddress,
+                .CustomerGroup = "Residential",
+                .ClientLanguage = "English",
+                .TinId = txtTinId.Text,
+                .ClientType = "Residential"
+            }
+
+            Dim success As Boolean = ClientController.CreateClient(client)
+
+            If success Then
+                MessageBox.Show("Client added successfully.")
+
+                txtClientName.Text = ""
+                txtPhoneNumber.Text = ""
+                txtClientEmail.Text = ""
+                txtAddress.Text = ""
+                txtCity.Text = ""
+                txtRegion.Text = ""
+                txtCountry.Text = ""
+                txtPostalCode.Text = ""
+                txtTinId.Text = ""
+
+
+            End If
         End Sub
     End Class
 End Namespace
