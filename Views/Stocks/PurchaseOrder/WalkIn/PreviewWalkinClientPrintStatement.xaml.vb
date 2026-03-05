05c664037985eb8d206044050f09f3f685b4735c
Andrews Leeo Matienzo <matienzoandrews@gmail.com>
2026-03-05 15:38:20 +0800

    delivery details was populated and used for loading delivery form instead of walkin billing details to avoid random inputs popping up

--- Views/Stocks/PurchaseOrder/WalkIn/PreviewWalkinClientPrintStatement.xaml.vb
+++ Views/Stocks/PurchaseOrder/WalkIn/PreviewWalkinClientPrintStatement.xaml.vb
@@ -400,6 +400,9 @@ Namespace DPC.Views.Stocks.PurchaseOrder.WalkIn
                 ' Handle the response
                 If result = MessageBoxResult.Yes Then
                     _showDelivery = True
+                    DeliveryDetails.DRReferenceInvoice = WalkinBillingStatementDetails.BLNumberCache
+                    DeliveryDetails.DRClientName = WalkinBillingStatementDetails.BLClientName
+                    WalkinBillingStatementDetails.ClearAllBLCache()
                     ViewLoader.DynamicView.NavigateToView("newdelivery", Me)
                 Else
                     Debug.WriteLine("User declined Delivery Receipt creation.")
