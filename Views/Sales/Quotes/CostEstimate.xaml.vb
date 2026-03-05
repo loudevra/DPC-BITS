12889c9ec4d0c877c5e50b081f08bad9a1fe2a49
Andrews Leeo Matienzo <matienzoandrews@gmail.com>
2026-03-05 16:42:59 +0800

    delivery/mobilization text now transfers to print preview, billing preview with extra peso sign was fixed

--- Views/Sales/Quotes/CostEstimate.xaml.vb
+++ Views/Sales/Quotes/CostEstimate.xaml.vb
@@ -302,6 +302,7 @@ Namespace DPC.Views.Sales.Quotes
             CostEstimateDetails.CEGrandTotalCost = TotalCost.Text
             CostEstimateDetails.CEApproved = cmbApproved.Text
             CostEstimateDetails.CEWarranty = Warranty.Text
+            CostEstimateDetails.CEDeliveryMobilization = cmbDeliveryMobilization.Text
 
             Dim deliv As Decimal = 0
             Dim install As Decimal = 0
