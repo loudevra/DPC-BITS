163ff324041d7ac8f1d0a7ba8320dcb04ef33600
Clark Bernandino <clarkings14@gmail.com>
2026-04-10 09:33:44 +0800

    create another release

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -52,5 +52,5 @@ Imports System.Windows
 '      Revision
 '
 
-<Assembly: AssemblyVersion("2.1.0.37")>
-<Assembly: AssemblyFileVersion("1.0.7.0")>
+<Assembly: AssemblyVersion("2.1.0.39")>
+<Assembly: AssemblyFileVersion("1.0.8.0")>
