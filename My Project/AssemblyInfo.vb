a898304c90a8c589bde183047acf99da3288b05f
Clark Bernandino <clarkings14@gmail.com>
2026-05-19 19:14:10 +0800

    update for inserting either not type a 0

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -52,5 +52,5 @@ Imports System.Windows
 '      Revision
 '
 
-<Assembly: AssemblyVersion("2.1.0.42")>
-<Assembly: AssemblyFileVersion("1.0.11.0")>
+<Assembly: AssemblyVersion("2.1.0.43")>
+<Assembly: AssemblyFileVersion("1.0.12.0")>
